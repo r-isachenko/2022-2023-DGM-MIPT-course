88cb893f05bd3d8f5c3312c1b2a80d1ca3b0a88a
r-isachenko <isa-ro@yandex.ru>
2023-01-31 18:37:46 +0300

    added load_dataset function

--- homeworks/dgm_utils/data.py
+++ homeworks/dgm_utils/data.py
@@ -2,6 +2,8 @@ import numpy as np
 import pickle
 from typing import Tuple
 
+import torchvision
+
 
 def load_pickle(
     path: str, flatten: bool = False, binarize: bool = False
@@ -21,4 +23,58 @@ def load_pickle(
     if flatten:
         train_data = train_data.reshape(len(train_data.shape[0]), -1)
         test_data = test_data.reshape(len(train_data.shape[0]), -1)
+    return train_data, test_data
+
+
+def load_MNIST() -> Tuple[np.ndarray, np.ndarray]:
+    train_data = torchvision.datasets.MNIST(root="./", train=True, download=True)
+    test_data = torchvision.datasets.MNIST(root="./", train=True, download=True)
+    train_data, test_data = train_data.data.numpy(), test_data.data.numpy()
+    axis_index = len(train_data.shape)
+    train_data = np.expand_dims(train_data, axis=axis_index)
+    test_data = np.expand_dims(test_data, axis=axis_index)
+
+    return train_data, test_data
+
+
+def load_CIFAR10() -> Tuple[np.ndarray, np.ndarray]:
+    train_data = torchvision.datasets.CIFAR10(root="./", train=True, download=True)
+    test_data = torchvision.datasets.CIFAR10(root="./", train=False, download=True)
+    train_data, test_data = train_data.data, test_data.data
+
+    return train_data, test_data
+
+
+def _load_dataset(name: str) -> Tuple[np.ndarray, np.ndarray]:
+    if name == "mnist":
+        return load_MNIST()
+    elif name == "cifar10":
+        return load_CIFAR10()
+    else:
+        raise ValueError("The argument name must have the values 'mnist' or 'cifar10'")
+
+
+def load_dataset(
+    name: str, flatten: bool = False, binarize: bool = True
+) -> Tuple[np.ndarray, np.ndarray]:
+
+    train_data, test_data = _load_dataset(name)
+
+    train_data = train_data.astype("float32")
+    test_data = test_data.astype("float32")
+
+    if binarize:
+        train_data = (train_data > 128).astype("float32")
+        test_data = (test_data > 128).astype("float32")
+    else:
+        train_data = train_data / 255.0
+        test_data = test_data / 255.0
+
+    train_data = np.transpose(train_data, (0, 3, 1, 2))
+    test_data = np.transpose(test_data, (0, 3, 1, 2))
+
+    if flatten:
+        train_data = train_data.reshape(len(train_data.shape[0]), -1)
+        test_data = test_data.reshape(len(train_data.shape[0]), -1)
+
     return train_data, test_data
